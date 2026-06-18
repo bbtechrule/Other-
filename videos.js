46e52cb19f89fac787ba79ee2f5709223ee05aca
bbtechrule <bbtechrule@gmail.com>
2026-06-18 12:23:00 +0530

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,9 @@
+const videos = [
+
+"https://res.cloudinary.com/dujoufris/video/upload/v1781713943/VID_20260617_090426_628_dm00bf.mp4",
+
+"https://your-next-video.mp4",
+
+"https://your-third-video.mp4"
+
+];
