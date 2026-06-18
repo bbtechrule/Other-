26f5601121610520ca7055f2af8cf163dfb5cf15
bbtechrule <bbtechrule@gmail.com>
2026-06-18 12:27:02 +0530

    Delete images.js

--- images.js
+++
@@ -1,9 +0,0 @@
-const images = [
-
-"https://res.cloudinary.com/dujoufris/image/upload/v1781711779/file_00000000aa3071fa9a302b75ebfe6ef7_dltshu.png",
-
-"https://your-next-image.jpg",
-
-"https://your-third-image.jpg"
-
-];
