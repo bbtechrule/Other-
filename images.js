99a1d12712b0aecd58db8de1a740a2fc6f448af3
bbtechrule <bbtechrule@gmail.com>
2026-06-18 18:14:32 +0530

    Update images.js

--- images.js
+++ images.js
@@ -2,7 +2,7 @@ const images = [
 
 "https://res.cloudinary.com/dujoufris/image/upload/v1781711779/file_00000000aa3071fa9a302b75ebfe6ef7_dltshu.png",
 
-"https://res.cloudinary.com/dujoufris/image/upload/v1781782469/image_vctbmi.png,
+"https://your-next-image.jpg",
 
 "https://your-third-image.jpg"
 
